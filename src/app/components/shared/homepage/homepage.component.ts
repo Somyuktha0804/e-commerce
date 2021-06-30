035533be4fea76d1ac2c0a8bd9a363a2be57d74e
Somyuktha0804 <86700677+Somyuktha0804@users.noreply.github.com>
2021-06-30 12:34:57 +0530

    Add files via upload
    homepage files

---
+++ src/app/components/shared/homepage/homepage.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  slider1='../assets/images/others/slider1.jpg'
+  slider2='../assets/images/others/slider2.jpg'
+  slider3='../assets/images/others/slider3.jpg'
+  slider4='../assets/images/others/advertisement.jfif'
+ 
+
+
+}
